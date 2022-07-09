2e9070cb17bf2deedf342b3e3048e03c39eea19c
Harley-ca <92524806+harley-ca@users.noreply.github.com>
2022-07-09 14:51:18 +1000

    Fixed the internal 500
    Apparently when we did this in class, you had to use credentials, but for some reason now it only works with secrets.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,19 @@
 class UsersController < ApplicationController
 
-    def create
-        @user = User.create(user_params)
 
-        if @user.errors.any?
-            render json: @user.errors, status: :unprocessable_entity
+    def create
+        @user = User.new(user_params)
+        @user.isAdmin = false
+
+        if @user.save
+            print @user
+            auth_token = Knock::AuthToken.new payload: {sub: @user.id}
+            render json: {username: @user.username, jwt: auth_token.token }, status: :created #, location: @message
         else
-            auth_token = Knock::AuthToken.new payload: { sub: @user.id }
-            render json: {username: @user.username, jwt: auth_token.token}, status: :created
+            # render json: @user.errors, status: :unprocessable_entity
+            print @user.errors.attribute_names
+            # render json: {error: "Invalid signup"}
+            render json: {error: @user.errors}
         end
     end
 
@@ -15,6 +21,6 @@ class UsersController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def user_params
-        params.permit(:username, :email, :rank, :password, :password_confirmation)
+        params.permit(:username, :email, :user, :password, :password_confirmation)
     end
 end
